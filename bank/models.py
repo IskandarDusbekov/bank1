c0e571062120dff42ff89f54c589adee887a4025
AlexandreLeGrand1 <iskandardusbekov1@gmail.com>
2026-02-13 17:32:56 +0500

    Models
    -urls.py fayli qoshilda apga
    -Card modeli tuzildi

--- bank/models.py
+++ bank/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+class Card(models.Model):
+    """
+    Card Model
+    """
+    S_Active="active"
+    S_Inactive="inactive"
+    S_Expired="expired"
+
+    STATUS_CHOICES = [
+        (S_Active, "Active"),
+        (S_Inactive, "Inactive"),
+        (S_Expired, "Expired"),
+    ]
+
+    card_number = models.CharField(max_length=16, unique=True)
+    phone =models.CharField(max_length=16,null=True,blank=True)
+    expire = models.DateField()
+    status = models.CharField(choices=STATUS_CHOICES, max_length=10)
+    balance = models.DecimalField(decimal_places=2, max_digits=20)
+
+    def __str__(self):
+        return self.card_number
+    
---
+++ trash/documentation.md
@@ -0,0 +1,15 @@
+# Bank systm
+
+## Bank sistemasini qurish 
+
+### 1-task
+
+- Exel fayl yaratish
+
+#### Card nomili model yasaldi
+
+- card_number
+- expire
+- phone 
+- balance
+- status
